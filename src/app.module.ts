d0175622390a9c61d806e6ff3852f096d1fd7f80
AlexandreAdabo <alexandreadabo_@hotmail.com>
2025-01-20 07:35:31 -0300

    first commit prismaapi

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ],
 })
 export class AppModule {}
---
+++ src/users/repositories/users.repository.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from '../dto/create-user.dto';
+import { UpdateUserDto } from '../dto/update-user.dto';
+import { UserEntity } from '../entities/user.entity';
+
+@Injectable()
+export class UsersRepository {
+  constructor(private readonly prisma: PrismaService) {}
+  create(createUserDto: CreateUserDto): Promise<UserEntity> {
+    return this.prisma.user.create({
+      data: createUserDto,
+    });
+  }
+
+  findAll(): Promise<UserEntity[]> {
+    return this.prisma.user.findMany({});
+  }
+
+  findOne(id: number): Promise<UserEntity> {
+    return this.prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto): Promise<UserEntity> {
+    return this.prisma.user.update({
+      where: {
+        id,
+      },
+      data: updateUserDto,
+    });
+  }
+
+  remove(id: number): Promise<UserEntity> {
+    return this.prisma.user.delete({
+      where: {
+        id,
+      },
+    });
+  }
+}
